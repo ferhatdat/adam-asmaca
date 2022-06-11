06ec9cf3e9afd719549a54a97b46a1c1e3fc0a3c
ferhatdat <ferhatgul466@gmail.com>
2022-06-11 20:40:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+const word_el = document.getElementById('word')
+const popup = document.getElementById('popup-container')
+const message = document.getElementById('success-message')
+const wrongLetters_el = document.getElementById('wrong-letters')
+const items = document.querySelectorAll('.item')
+const message_el = document.getElementById('message')
+const PlayAgainBtn = document.getElementById('play-again')
+
+const correctLetters = [];
+const wrongLetters = [];
+
+const getRandomWord = () => {
+    const words = ["javascript", "java", "python", "ferhat", "hulya", "cihan"]
+
+    return words[Math.floor(Math.random() * words.length)]
+}
+let selectedWord = getRandomWord();
+
+const displayWord = () => {   
+    word_el.innerHTML = `
+    ${selectedWord.split('').map(letter => `
+    <div class=letter>${correctLetters.includes(letter) ? letter : ''}</div>`).join('')}
+    `;
+    const w = word_el.innerText.replace(/\n/g, '')
+    if (w === selectedWord){
+        popup.style.display = "flex"
+        message.innerText='Tebrikler Kazandınız.'
+    }
+}
+
+function updateWrongLetters(){
+    wrongLetters_el.innerHTML = `
+    ${wrongLetters.length > 0 ? '<h3>Hatalı Harfler</h3>' : ''}
+    ${wrongLetters.map(letter => `
+    <span>${letter}</span>
+    `)}
+    `;
+    items.forEach((item, index) => {
+        const errorCount = wrongLetters.length;
+        if(index<errorCount){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none'
+        }
+    })
+
+    if (wrongLetters.length === items.length){
+        popup.style.display = 'flex';
+        message.innerText = 'Maalesef Kaybettiniz.'
+    }
+}
+
+const displayMessage = () => {
+    message_el.classList.add("show")
+
+    setTimeout(() => {
+        message_el.classList.remove("show")
+    }, 2000);
+}
+
+
+PlayAgainBtn.addEventListener('click', function() {
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = getRandomWord()
+    displayWord()
+    updateWrongLetters()
+    popup.style.display = 'none'
+})
+
+window.addEventListener('keydown', function(e) {
+    if (e.keyCode >= 65 && e.keyCode <=90){
+        const letter = e.key
+
+        if (selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter)
+                displayWord();
+            } else {
+                displayMessage()
+            }
+        } else {
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter)
+                updateWrongLetters()
+            } else {
+                displayMessage()
+            }
+        }
+    }
+})
+
+displayWord()
